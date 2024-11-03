e2bfc1b039a585dec5e8c5f8fa92623f1065eff9
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2024-11-03 11:43:36 +0100

    better plotting functions

--- src/erispy/nix/plotting.py
+++ src/erispy/nix/plotting.py
@@ -6,27 +6,33 @@ from astropy.visualization import (PercentileInterval, AsinhStretch, ImageNormal
 
 from .data import get_science_data
 
-def plot_file_frame(file, output_folder='./figures/',  frame=0):
+def plot_file_frame(file, output_folder='./figures/', frame=0, percentile=99.5, cmap='gray'):
 
+	#output name
 	output_name = output_folder + os.path.basename(file)[:-5] + "_frame_{}.png".format(frame)
+	
+	#fetch data
 	data = get_science_data(file).astype(np.float32)
 	image = data[frame]
-	upper_limit = np.percentile(np.nan_to_num(image, nan=0.0), 99.95)
+	
+	stretch = AsinhStretch()  
+	interval = PercentileInterval(percentile)
+
+	imshow_settings = { 
+		'cmap' : cmap, 
+		'norm': ImageNormalize(data, interval=interval, stretch=stretch), 
+		'origin': 'lower'}
 
 	fig, ax = plt.subplots(figsize=(10, 10),dpi=300)
-	imshow_settings = { 'cmap' : 'cubehelix', 'norm': LogNorm(vmin=0.99, vmax=upper_limit), 'origin': 'lower'}
-
 	ax.imshow(image, **imshow_settings)
 	ax.set_title("Source Image")
 	fig.savefig(output_name)
 	plt.close()
-
-def plot_data_with_zoomin(data,x0,y0,w,percentile=99.95, cmap='gray'):
 	
-	cmap = plt.cm.gray  # Choose a colormap suitable for astronomical data
-	cmap.set_bad(color='black')  # Optionally, set NaN color to black
+	return
 
-	#stretch = AsinhStretch()  # Try LogStretch, SqrtStretch, etc. to see different effects
+def plot_data_with_zoomin(data,x0,y0,w,percentile=99.5, cmap='cubehelix'):
+	
 	stretch = LogStretch()#, SqrtStretch, etc. to see different effects
 	interval = PercentileInterval(percentile)  # Cuts off outliers at the extremes
 
@@ -45,7 +51,7 @@ def plot_data_with_zoomin(data,x0,y0,w,percentile=99.95, cmap='gray'):
 	return fig, (ax1,ax2)
 
 
-def plot_in_axis(ax,data, percentile=99.95, cmap='gray'):
+def plot_in_axis(ax,data, percentile=99.5, cmap='cubehelix'):
 	
 	stretch = AsinhStretch()  # Try LogStretch, SqrtStretch, etc. to see different effects
 	interval = PercentileInterval(percentile)  # Cuts off outliers at the extremes
